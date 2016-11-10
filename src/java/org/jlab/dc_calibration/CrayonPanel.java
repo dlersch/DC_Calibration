246169be3911445ecf5328a5c4c18d848bdedbf8
mckunkel <mkunkel@jlab.org>
2016-11-10 17:30:38 -0500

    Fixed not needed imports. Also put Krishna's println back to System.out.println

--- src/java/org/jlab/dc_calibration/CrayonPanel.java
+++ src/java/org/jlab/dc_calibration/CrayonPanel.java
@@ -27,118 +27,123 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
+ */
 
-package dc_calibration;
+package java.org.jlab.dc_calibration;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.event.*;
-import javax.swing.colorchooser.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.border.Border;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
 
 /* Used by DC_calibration.java. */
-public class CrayonPanel extends AbstractColorChooserPanel
-                         implements ActionListener {
-    JToggleButton redCrayon;
-    JToggleButton yellowCrayon;
-    JToggleButton greenCrayon;
-    JToggleButton blueCrayon;
+public class CrayonPanel extends AbstractColorChooserPanel implements ActionListener {
+	JToggleButton redCrayon;
+	JToggleButton yellowCrayon;
+	JToggleButton greenCrayon;
+	JToggleButton blueCrayon;
 
-    public void updateChooser() {
-        Color color = getColorFromModel();
-        if (Color.red.equals(color)) {
-            redCrayon.setSelected(true);
-        } else if (Color.yellow.equals(color)) {
-            yellowCrayon.setSelected(true);
-        } else if (Color.green.equals(color)) {
-            greenCrayon.setSelected(true);
-        } else if (Color.blue.equals(color)) {
-            blueCrayon.setSelected(true);
-        }
-    }
+	public void updateChooser() {
+		Color color = getColorFromModel();
+		if (Color.red.equals(color)) {
+			redCrayon.setSelected(true);
+		} else if (Color.yellow.equals(color)) {
+			yellowCrayon.setSelected(true);
+		} else if (Color.green.equals(color)) {
+			greenCrayon.setSelected(true);
+		} else if (Color.blue.equals(color)) {
+			blueCrayon.setSelected(true);
+		}
+	}
 
-    protected JToggleButton createCrayon(String name,
-                                         Border normalBorder) {
-        JToggleButton crayon = new JToggleButton();
-        crayon.setActionCommand(name);
-        crayon.addActionListener(this);
+	protected JToggleButton createCrayon(String name, Border normalBorder) {
+		JToggleButton crayon = new JToggleButton();
+		crayon.setActionCommand(name);
+		crayon.addActionListener(this);
 
-        //Set the image or, if that's invalid, equivalent text.
-        ImageIcon icon = createImageIcon("images/" + name + ".gif");
-        if (icon != null) {
-            crayon.setIcon(icon);
-            crayon.setToolTipText("The " + name + " crayon");
-            crayon.setBorder(normalBorder);
-        } else {
-            crayon.setText("Image not found. This is the "
-                           + name + " button.");
-            crayon.setFont(crayon.getFont().deriveFont(Font.ITALIC));
-            crayon.setHorizontalAlignment(JButton.HORIZONTAL);
-            crayon.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        }
+		// Set the image or, if that's invalid, equivalent text.
+		ImageIcon icon = createImageIcon("images/" + name + ".gif");
+		if (icon != null) {
+			crayon.setIcon(icon);
+			crayon.setToolTipText("The " + name + " crayon");
+			crayon.setBorder(normalBorder);
+		} else {
+			crayon.setText("Image not found. This is the " + name + " button.");
+			crayon.setFont(crayon.getFont().deriveFont(Font.ITALIC));
+			crayon.setHorizontalAlignment(JButton.HORIZONTAL);
+			crayon.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		}
 
-        return crayon;
-    }
+		return crayon;
+	}
 
-    protected void buildChooser() {
-        setLayout(new GridLayout(0, 1));
+	protected void buildChooser() {
+		setLayout(new GridLayout(0, 1));
 
-        ButtonGroup boxOfCrayons = new ButtonGroup();
-        Border border = BorderFactory.createEmptyBorder(4,4,4,4);
+		ButtonGroup boxOfCrayons = new ButtonGroup();
+		Border border = BorderFactory.createEmptyBorder(4, 4, 4, 4);
 
-        redCrayon = createCrayon("red", border);
-        boxOfCrayons.add(redCrayon);
-        add(redCrayon);
+		redCrayon = createCrayon("red", border);
+		boxOfCrayons.add(redCrayon);
+		add(redCrayon);
 
-        yellowCrayon = createCrayon("yellow", border);
-        boxOfCrayons.add(yellowCrayon);
-        add(yellowCrayon);
+		yellowCrayon = createCrayon("yellow", border);
+		boxOfCrayons.add(yellowCrayon);
+		add(yellowCrayon);
 
-        greenCrayon = createCrayon("green", border);
-        boxOfCrayons.add(greenCrayon);
-        add(greenCrayon);
+		greenCrayon = createCrayon("green", border);
+		boxOfCrayons.add(greenCrayon);
+		add(greenCrayon);
 
-        blueCrayon = createCrayon("blue", border);
-        boxOfCrayons.add(blueCrayon);
-        add(blueCrayon);
-    }
+		blueCrayon = createCrayon("blue", border);
+		boxOfCrayons.add(blueCrayon);
+		add(blueCrayon);
+	}
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = CrayonPanel.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
-    }
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected static ImageIcon createImageIcon(String path) {
+		java.net.URL imgURL = CrayonPanel.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
 
-    public void actionPerformed(ActionEvent e) {
-        Color newColor = null;
-        String command = ((JToggleButton)e.getSource()).getActionCommand();
-        if ("green".equals(command))
-            newColor = Color.green;
-        else if ("red".equals(command))
-            newColor = Color.red;
-        else if ("yellow".equals(command))
-            newColor = Color.yellow;
-        else if ("blue".equals(command))
-            newColor = Color.blue;
-        getColorSelectionModel().setSelectedColor(newColor);
-    }
+	public void actionPerformed(ActionEvent e) {
+		Color newColor = null;
+		String command = ((JToggleButton) e.getSource()).getActionCommand();
+		if ("green".equals(command))
+			newColor = Color.green;
+		else if ("red".equals(command))
+			newColor = Color.red;
+		else if ("yellow".equals(command))
+			newColor = Color.yellow;
+		else if ("blue".equals(command))
+			newColor = Color.blue;
+		getColorSelectionModel().setSelectedColor(newColor);
+	}
 
-    public String getDisplayName() {
-        return "Crayons";
-    }
+	public String getDisplayName() {
+		return "Crayons";
+	}
 
-    public Icon getSmallDisplayIcon() {
-        return null;
-    }
+	public Icon getSmallDisplayIcon() {
+		return null;
+	}
 
-    public Icon getLargeDisplayIcon() {
-        return null;
-    }
+	public Icon getLargeDisplayIcon() {
+		return null;
+	}
 }
--- src/java/org/jlab/dc_calibration/Echo.java
+++ src/java/org/jlab/dc_calibration/Echo.java
@@ -3,34 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dc_calibration;
+package java.org.jlab.dc_calibration;
 
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+
 import javax.swing.JTextArea;
 
 /**
  *
  * @author KPAdhikari
  */
-//http://stackoverflow.com/questions/4422642/java-console-jpanel
+// http://stackoverflow.com/questions/4422642/java-console-jpanel
 public class Echo extends FilterOutputStream {
-        private final JTextArea text;
+	private final JTextArea text;
 
-    public Echo(OutputStream out, JTextArea text) {
-     //public Echo( DC_calibration  out, JTextArea text) {
-        super(out);
-        if (text == null) throw new IllegalArgumentException("null text");
-        this.text = text;
-    }
+	public Echo(OutputStream out, JTextArea text) {
+		// public Echo( DC_calibration out, JTextArea text) {
+		super(out);
+		if (text == null)
+			throw new IllegalArgumentException("null text");
+		this.text = text;
+	}
 
-    @Override
-    public void write(int b) throws IOException {
-        super.write(b);
-        text.append(Character.toString((char) b));
-        // scroll to end?
-    }
+	@Override
+	public void write(int b) throws IOException {
+		super.write(b);
+		text.append(Character.toString((char) b));
+		// scroll to end?
+	}
 
-    // overwrite the other write methods for better performance
+	// overwrite the other write methods for better performance
 }
